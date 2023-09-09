412f617382264b09fa47e5dcc5274d36b8a29c82
Dog2657 <reeceya2006@gmail.com>
2023-09-09 04:26:50 +0100

    Added share modal

---
+++ app/static/albemClient/main.js
@@ -0,0 +1,25 @@
+{//Copy link init
+    const containor = document.querySelector('dialog[data-name=share] > section.Link')
+    const text = document.createTextNode(window.location.href);
+    const button = document.createElement('button')
+
+    button.textContent = "Copy"
+    button.onclick = async() => {
+        try{
+            await navigator.clipboard.writeText(window.location.href)
+        } catch (error){
+            console.error("Failed to set clipboard:", error)
+        }
+    }
+
+    containor.append(text)
+    containor.append(button)
+}
+var qrcode = new QRCode(document.querySelector('dialog[data-name=share] > section.QR-Code'), {
+    text: "window.location.href",
+    colorDark: "#000000",
+    colorLight: "transparent",
+    correctLevel: QRCode.CorrectLevel.H
+});
+
+document.querySelector('dialog').showModal()
